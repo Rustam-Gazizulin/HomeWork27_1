40c9f1b9278cc82296d4e76553ce0a51ab8ef66d
rgazizulin174 <rgazizulin174@gmail.com>
2022-09-25 17:47:21 +0500

    add view fix admin

--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,6 @@
 import json
 
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
