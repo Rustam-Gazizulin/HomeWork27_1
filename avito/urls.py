309a959c56557bb00aa7a9ad5562f0713c6b362b
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-19 18:16:43 +0500

    add DRF, Pagination = 5

--- avito/urls.py
+++ avito/urls.py
@@ -23,6 +23,7 @@ from ads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', views.root),
     path('cat/', include('ads.urls.cat_urls')),
     path('ad/', include('ads.urls.ad_urls')),
