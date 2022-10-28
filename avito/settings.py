2799b3517773548bcc9a4aea54f7921f14c18cec
rgazizulin174 <rgazizulin174@gmail.com>
2022-10-28 18:00:12 +0500

    add JWTauthentification

--- avito/settings.py
+++ avito/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'ads',
     'users',
 ]
@@ -137,6 +138,9 @@ TOTAL_ON_PAGE = 5
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
 }
 
 AUTH_USER_MODEL = 'users.User'
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users import views
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('update/<int:pk>/', views.UserUpdateView.as_view()),
     path('create/', views.UserCreateView.as_view()),
     path('delete/<int:pk>/', views.UserDeleteView.as_view()),
+
+    # Урлы аутентификации
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
